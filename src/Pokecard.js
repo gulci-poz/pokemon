445253a920d9dace2448d914708d7ac3bdc062e3
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2019-05-20 05:40:19 +0200

    Pokegame component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => number <= 999 ? `00${number}`.slice(-3) : number;
 
 class Pokecard extends Component {
     render() {
         const {id, name, type, exp} = this.props;
-        const imgSrc = `${POKE_API}${id}.png`;
+        // const imgSrc = `${POKE_API}${id}.png`;
+        const imgSrc = `${POKE_API}${padToThree(id)}.png`;
 
         return (
             <div className="Pokecard">
